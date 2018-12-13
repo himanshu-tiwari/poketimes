914d29688388a407244f32896040143f47f4556f
himanshu-tiwari <htiwarih@gmail.com>
2018-12-13 15:54:15 +0530

    Making programmatic redirects using history and withRouter in non-routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+	setTimeout(() => {
+		props.history.push('/');
+	}, 2000);
+
 	return(
 		<nav className="nav-wrapper red darken-3">
 			<div className="container">
@@ -17,4 +21,4 @@ const Navbar = () => {
 	);
 };
 
-export default Navbar;
+export default withRouter(Navbar);
