3e3639effea15a2cdd3ae9130f19557f457061d8
himanshu-tiwari <htiwarih@gmail.com>
2018-12-13 15:38:06 +0530

    using navlinks to avoid page relaod on click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 	return(
@@ -7,9 +8,9 @@ const Navbar = () => {
 				<a href='/' className="brand-logo">Poke'Times</a>
 
 				<ul className="right">
-					<li><a href="/">Home</a></li>
-					<li><a href="/about">About</a></li>
-					<li><a href="/contact">Contact</a></li>
+					<li><NavLink to="/">Home</NavLink></li>
+					<li><NavLink to="/about">About</NavLink></li>
+					<li><NavLink to="/contact">Contact</NavLink></li>
 				</ul>
 			</div>
 		</nav>
